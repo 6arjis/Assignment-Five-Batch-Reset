16199ecd08c89f37933fee0ace727cbdd29121fc
6arjis <barjjishasanchowdhury96@gmail.com>
2024-12-07 00:40:39 +0600

    done

--- js/home.js
+++ js/home.js
@@ -1,16 +1,5 @@
-// document.getElementById('btn-switch-blog').addEventListener("click",()=> {
-//   window.location.href = "/blog.html";
-// });
-
-// // document.getElementById('switch-home').addEventListener('click',()=>{
-// //     window.location.href = "/index.html";
-// // })
-// // document.getElementById('switch-home').addEventListener('click', () => {
-// //     window.location.href = "/";
-// //   });
 
 let popup = document.getElementById('popup');
-
 
 document.getElementById('switch-history').addEventListener("click",()=>{
     document.getElementById('switch-donation').style.backgroundColor="lightGrey"
